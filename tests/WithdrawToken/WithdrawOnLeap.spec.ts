62900ccc43f2134ca6db7e8c35252f40d1e93eea
KimieTran <34021222+KimieTran@users.noreply.github.com>
2025-03-12 00:28:50 +0700

    add withdraw DEPOSIT_UI_V2_08 with Leap wallet

--- tests/WithdrawToken/WithdrawOnLeap.spec.ts
+++ tests/WithdrawToken/WithdrawOnLeap.spec.ts
@@ -6,15 +6,16 @@ import { ConnectWalletPage } from '../../pages/ConnectWalletPage';
 import { PhantomPage } from '../../pages/PhantomPage';
 import { DepositPage } from '../../pages/DepositPage';
 import { WithdrawPage } from '../../pages/WithdrawPage';
+import { LeapPage } from '../../pages/LeapPage';
 
 let page: Page
 let browserContext: BrowserContext
-const srpArr = ["doll", "injury", "material", "wise", "matrix", "pet", "debate", "asset", "forest", "online", "toss", "holiday"]
-const password = 'Abc123456789'
-const extensionName = "phantom"
-const leapSwthAddress = 'tswth1mw6zu5y7dxdhcrej4jqct95xxf7j5lgh6dyhfn'
+const srpArr = ["teach", "round", "olympic", "good", "avoid", "shiver", "cup", "attack", "program", "ride", "exotic", "either"]
+const passworld = 'Abc12345789'
+const extensionName = "leap"
+const phantomSwthAddress = 'tswth1r5kh40kknhw7e02qyzyz50zsfj8xyvfsq9lvxp'
 
-test.beforeAll('Add extension: Phantom', async () => {
+test.beforeAll('Add extension: Leap', async () => {
   test.setTimeout(120_000)
   browserContext = await createBrowserContext(extensionName)
   page = await browserContext.newPage()
@@ -23,61 +24,74 @@ test.beforeAll('Add extension: Phantom', async () => {
     browserContext.waitForEvent('page'),
   ]);
   await newPage.waitForLoadState()
+  await newPage.bringToFront()
   
-  const phantomPage = new PhantomPage(newPage)
-  await phantomPage.alreadyHaveWalletBtn.click()
-  await phantomPage.importSecretRecoveryPhraseBtn.click()
+  const leapPage = new LeapPage(newPage)
+  await leapPage.usingRecoverPhrase.click()
 
-  for (let i = 0; i < srpArr.length; i++) {
-    await phantomPage.secretRecoveryPhrases(i).fill(srpArr[i])
+  for (let i = 1; i <= srpArr.length; i++) {
+    if(i==1)
+      await leapPage.firstSecretRecoverPhrase.fill(srpArr[i-1])
+    else if(i==12){
+      await leapPage.secretRecoverPhrase12.click()
+      await leapPage.inputSRPTextbox.fill(srpArr[i-1])
+    }
+    else {
+      await leapPage.secretRecoveryPhrases(i).click()
+      await leapPage.inputSRPTextbox.fill(srpArr[i-1])
+    }
   }
-  await phantomPage.importWalletBtn.click()
-  await phantomPage.continueBtn.click()
-  await phantomPage.newPasswordTextBox.fill(password)
-  await phantomPage.confirmPasswordTextBox.fill(password)
-  await phantomPage.termServiceCheckbox.click()
-  await phantomPage.continueBtn.click()
-  //await phantomPage.submitBtn.click({ delay: 1000 })
+
+  await leapPage.importViaRecoverPhraseBtn.click()
+  await leapPage.importWalletBtn.click()
+  await leapPage.addWalletCheckbox1.click()
+  await leapPage.addWalletCheckbox2.click()
+  await leapPage.proceedBtn.click()
+  await leapPage.passwordTextbox.fill(passworld)
+  await leapPage.confirmPasswordTextbox.fill(passworld)
+  await leapPage.proceedBtn.click()
 
   await page.bringToFront()
-  test.setTimeout(180_000)
   const homePage = new HomePage(page)
   await homePage.goToHomePage()
-  page.waitForLoadState()
   expect(homePage.spotHistory).toBeTruthy();
   //await homePage.carbonTestnet.click();
   //await homePage.mantle.click()
   await page.waitForLoadState()
-
   await page.waitForTimeout(10_000)
+
   const tradePage = new TradeTradePage(page);
   await tradePage.headerConnectWallet.click();
 
   const connectWalletPage = new ConnectWalletPage(page)
   await connectWalletPage.selectWallet.isVisible()
-  await connectWalletPage.phantomBtn.waitFor({ state: 'visible' })
+
+  await connectWalletPage.leapBtn.waitFor({ state: 'visible' })
+  await connectWalletPage.leapBtn.click({ delay: 1000 })
+  const leapPage1 = new LeapPage(page)
+  await leapPage1.connectBtn.waitFor({ state: 'visible' })
 
   const [newPage1] = await Promise.all([
     browserContext.waitForEvent('page'),
-    await connectWalletPage.phantomBtn.click({ delay: 1000 })
+    await leapPage1.connectBtn.click({ delay: 1000 })
   ]);
   await newPage1.waitForLoadState()
 
-  const phantomPage1 = new PhantomPage(newPage1)
-  await phantomPage1.connectBtn.click({ delay: 1000 })
-
+  const approveLeapPage = new LeapPage(newPage1)
+  await approveLeapPage.approveBtn.waitFor({ state: 'visible' })
   const [newPage2] = await Promise.all([
     browserContext.waitForEvent('page'),
+    await approveLeapPage.approveBtn.click({ delay: 1000 })
   ]);
   await newPage2.waitForLoadState()
 
-  const phantomPage2 = new PhantomPage(newPage2)
-  await phantomPage2.connectBtn.waitFor({ state: 'visible' })
-  await phantomPage2.connectBtn.click({ delay: 1000 })
-  await homePage.addressPhantomDropBtn.waitFor({ state: 'visible' })
+  const approveLeapPage1 = new LeapPage(newPage2)
+  await approveLeapPage1.approveBtn.waitFor({ state: 'visible' })
+  await approveLeapPage1.approveBtn.click({ delay: 1000 })
+  await homePage.addressDropBtn.waitFor({ state: 'visible' })
 
 })
-test.describe(' Withdraw Token with Phantom wallet ', () => {
+test.describe(' Withdraw Token with Leap wallet ', () => {
     test('DEPOSIT_UI_V2_08: Check the active Withdraw button on the Deposit page', async () => {
         await page.reload()
         await page.waitForLoadState()
@@ -108,7 +122,7 @@ test.describe(' Withdraw Token with Phantom wallet ', () => {
         const withdrawPage = new WithdrawPage(page)
         await withdrawPage.selectToken.click()
         await withdrawPage.swthTokenOption.click()
-        await withdrawPage.recipientAddrTextbox.fill(leapSwthAddress)
+        await withdrawPage.recipientAddrTextbox.fill(phantomSwthAddress)
         await withdrawPage.amountTextbox.fill('1')
 
         const [popup] = await Promise.all([
@@ -117,10 +131,10 @@ test.describe(' Withdraw Token with Phantom wallet ', () => {
         ]);
         await popup.waitForLoadState()
 
-        const confirmPage = new PhantomPage(popup)
-        await confirmPage.connectBtn.waitFor({ state: 'visible' })
-        await confirmPage.connectBtn.click({ delay: 1000 })
-
+        const approvalPage = new LeapPage(popup)
+        await approvalPage.approveBtn.waitFor({ state: 'visible' })
+        await approvalPage.approveBtn.click({ delay: 1000 })
+    
         await expect(withdrawPage.transactionSuccess).toBeVisible()
     })
 
